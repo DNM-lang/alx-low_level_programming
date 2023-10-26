f39e632b45469c4e3eb80f0f3f2c53431e2ce8c9
DNM-lang <njorogedennis59@gmail.com>
2023-10-25 23:28:17 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of a string.
+ *
+ * @s : The string to be determined its length.
+ *
+ * Return: Length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+
+}
+
