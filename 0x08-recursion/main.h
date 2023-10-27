62d976bd0f44dafd54ae38c11df53398794e77db
DNM-lang <njorogedennis59@gmail.com>
2023-10-26 19:14:35 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,5 +15,7 @@ int _sqrt_recursion(int n);
 int actual_sqrt_recursion(int n, int i);
 int is_prime_number(int n);
 int actual_prime(int n, int i);
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
 
 #endif
