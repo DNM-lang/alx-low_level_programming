b266b0ec0ad5b8d6837b5def5fd501c60e266086
DNM-lang <njorogedennis59@gmail.com>
2023-10-28 11:14:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: Always zero(success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+	return (0);
+}
