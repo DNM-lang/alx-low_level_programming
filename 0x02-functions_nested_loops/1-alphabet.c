a2f818e12a2c1ef914fa4bef62c19442650cd925
DNM-lang <njorogedennis59@gmail.com>
2023-11-25 01:35:37 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always 0(success)
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
