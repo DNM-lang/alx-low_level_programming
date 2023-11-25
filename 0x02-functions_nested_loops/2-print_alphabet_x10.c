eb9e73689757fbc9839a7697bc7b5d030fc858de
DNM-lang <njorogedennis59@gmail.com>
2023-11-25 01:55:55 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ *
+ * Return: Always 0(succes)
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
+
