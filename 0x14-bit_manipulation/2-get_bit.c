1d1aa4ad66eadafcb9a4158eeaeb787ccd2c417d
DNM-lang <njorogedennis59@gmail.com>
2023-11-26 17:31:56 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,11 +7,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask = 1UL << index;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	unsigned long int mask = 1UL << index;
-
 	return ((n & mask) ? 1 : 0);
 }
